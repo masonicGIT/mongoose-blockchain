5dc92df141e6b706c063d04304901ddaf0e645f4
Mason Borda <masonborda@gmail.com>
2016-03-25 12:03:53 -0700

    House keeping

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var BitGoJS = require('bitgo');
 var Promise = require('bluebird');
 
-var ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-var bitgo = new BitGoJS.BitGo({env: 'test', accessToken: ACCESS_TOKEN});
+var BITGO_ACCESS_TOKEN = process.env.BITGO_ACCESS_TOKEN;
+var bitgo = new BitGoJS.BitGo({env: 'test', accessToken: BITGO_ACCESS_TOKEN});
 
 var authenticate = function() {
   return new Promise(function (resolve, reject) {
@@ -15,52 +15,53 @@ var authenticate = function() {
     });
   });
 };
-
-var bitcoin = function () {
-
-  var sendBitcoin = function(senderWallet, recipientAddress, amount, password) {
-    return new Promise(function (resolve, reject) {
-      senderWallet.sendCoins({ address: recipientAddress, amount: amount, walletPassphrase: password }, function(err, result) {
-        if (err) { reject(err) };
-        resolve({ transactionId: result.hash });
-      });
-    });  
-  };
-
-  var generateAddress = function(wallet) {
-    return new Promise(function (resolve, reject) {
-      wallet.createAddress({ "chain": 0 }, function(err, address) {
-        if (err) { reject(err) };
-        resolve({ address: address.address }); 
-      });
-    })
-  };
-
-  var getWallet = function(walletId) {
-    return new Promise(function (resolve, reject) {
-      bitgo.wallets().get({ "id": walletId }, function(err, wallet) {
-        if (err) { reject(err) };
-        resolve(wallet);
-      });
-    });
-  };
-
-  return {
-    getWallet: getWallet,
-    generateAddress: generateAddress
-  };
-}();
-
-var createUserWallet = function(password, label) {    
-  return new Promise(function (resolve, reject) {  
-    bitgo.wallets().createWalletWithKeychains({"passphrase": password, "label": label}, function(err, res) {
-      if (err) { reject(err) };
-      resolve(res);
-    });
-  });
-};
 			   
 var blockchain = function(schema) {    
+
+  var bitcoin = function () {
+
+    var sendBitcoin = function(senderWallet, recipientAddress, amount, password) {
+      return new Promise(function (resolve, reject) {
+        senderWallet.sendCoins({ address: recipientAddress, amount: amount, walletPassphrase: password }, function(err, result) {
+          if (err) { reject(err) };
+          resolve({ transactionId: result.hash });
+        });
+      });  
+    };
+
+    var generateAddress = function(wallet) {
+      return new Promise(function (resolve, reject) {
+        wallet.createAddress({ "chain": 0 }, function(err, address) {
+          if (err) { reject(err) };
+          resolve({ address: address.address }); 
+        });
+      })
+    };
+
+    var getWallet = function(walletId) {
+      return new Promise(function (resolve, reject) {
+        bitgo.wallets().get({ "id": walletId }, function(err, wallet) {
+          if (err) { reject(err) };
+          resolve(wallet);
+        });
+      });
+    };
+
+
+    var createUserWallet = function(password, label) {    
+      return new Promise(function (resolve, reject) {  
+        bitgo.wallets().createWalletWithKeychains({"passphrase": password, "label": label}, function(err, res) {
+          if (err) { reject(err) };
+          resolve(res);
+        });
+      });
+    };
+
+    return {
+      getWallet: getWallet,
+      generateAddress: generateAddress
+    };
+  }();
 
   schema.statics.deposit = Promise.coroutine(function* (username) {
     "use strict";
@@ -120,6 +121,12 @@ var blockchain = function(schema) {
 
     bitgo.wallets().createWalletWithKeychains({"passphrase": self.password, "label": self.username }, function(err, res) {
       if (err) {
+	 if (err.message === 'unauthorized') {
+	   console.log('\n\n** BitGo Access Token not set');
+           console.log('** Retreive your access token from https://www.bitgo.com and set it as an environment variable');
+           console.log("** export BITGO_ACCESS_TOKEN='J3Das83k..3laelkwasd'");
+	   console.log('** Join us on Slack if you continue to experience issues https://slack.bitgo.com');
+	 }
 	 throw new Error(err);
       }
       self.bitcoin = { walletId: res.wallet.wallet.id };
